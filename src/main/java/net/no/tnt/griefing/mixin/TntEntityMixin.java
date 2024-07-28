e85b3250a8cddb6a5605f0c19ff3ae8ed42ccb57
Flooflez <maxmyta@gmail.com>
2024-07-28 21:45:04 +0800

    update to 1.21

--- src/main/java/net/no/tnt/griefing/mixin/TntEntityMixin.java
+++ src/main/java/net/no/tnt/griefing/mixin/TntEntityMixin.java
@@ -17,8 +17,8 @@ public abstract class TntEntityMixin extends Entity {
 		super(type, world);
 	}
 
-	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"), index = 5)
-	private World.ExplosionSourceType modifyExplosionSourceType(World.ExplosionSourceType destructionType) {
+	@ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"), index = 8)
+	private World.ExplosionSourceType modifyExplosionSourceType(World.ExplosionSourceType explosionSourceType) {
 		GameRules gameRules = this.getWorld().getGameRules();
 		if (!gameRules.getBoolean(NoTNTGriefing.TNT_GRIEFING)) {
 			return World.ExplosionSourceType.NONE;
